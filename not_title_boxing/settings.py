509c88307e3fc605b24487b4ff2e63d67b79ce4b
Nick Berejan <nberejan@lexile.com>
2020-04-22 10:17:55 -0400

    outlines of the models

--- not_title_boxing/settings.py
+++ not_title_boxing/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'not_title_boxing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME', "not_title_boxing"),
+        'USER': env('DB_USER', 'root'),
+        'PASSWORD': env('DB_PASSWORD', ''),
+        'HOST': env('DB_HOST', ''),
+        'PORT': env('DB_PORT', '')
     }
 }
 
