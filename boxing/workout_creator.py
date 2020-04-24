847d00516f284d91e6662c3be93c1cca387ea0f2
Nick Berejan <nberejan@lexile.com>
2020-04-24 10:42:03 -0400

    added admin, first migration

---
+++ boxing/workout_creator.py
@@ -0,0 +1,18 @@
+
+from boxing.models import WorkOut, WarmUp, Round, CorePeriod
+
+def create_workout():
+    pass
+
+
+def create_warm_up():
+    pass
+
+def create_rounds():
+    pass
+
+def create_round():
+    pass
+
+def create_core_period():
+    pass
